4d0aadc680c3f122e1b862fd92d9bc7e937c3261
Santiago Grangetto <santiagograngetto@gmail.com>
2018-07-16 21:28:06 -0300

    Add intent to open activities
    Add intent in main for each activity to be open from main activity

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View v) {
+        Intent numbersList = new Intent(this, NumbersActivity.class);
+        startActivity(numbersList);
+    }
+
+    public void openFamilyList(View v) {
+        Intent familylist = new Intent(this, FamilyActivity.class);
+        startActivity(familylist);
+    }
+
+    public void openColorsList(View v) {
+        Intent colorsList = new Intent(this, ColorsActivity.class);
+        startActivity(colorsList);
+    }
+
+    public void openPhrasesList(View v) {
+        Intent phrasesList = new Intent(this, PhrasesActivity.class);
+        startActivity(phrasesList);
     }
 }
