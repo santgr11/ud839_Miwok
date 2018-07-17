c6c48152db084fc6fb0910eff72241daf6b40afa
Santiago Grangetto <santiagograngetto@gmail.com>
2018-07-16 23:24:50 -0300

    Add number words to NumbersActivity
    Add a text view for each number word in the layout Numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create array of words containing every number from 1 to 10
+        ArrayList<String> words = new ArrayList<String>();
+        Collections.addAll(words, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        //Create a child TextView inside the main layout with every number
+        for(String word : words) {
+            TextView wordView = new TextView(this);
+            wordView.setText(word);
+            rootView.addView(wordView);
+        }
+
     }
 }
