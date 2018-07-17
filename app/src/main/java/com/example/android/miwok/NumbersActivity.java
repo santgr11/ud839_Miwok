81873991c61619e5299e8547aaa4fb005305168e
Santiago Grangetto <santiagograngetto@gmail.com>
2018-07-17 00:42:29 -0300

    Create custom layout for activities
    Create custom layout to display the words with their translation

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -22,7 +22,7 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<String> words = new ArrayList<String>();
         Collections.addAll(words, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.list_item, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
